f52b22f4ac02c32c142a08b80e7193ee82fdc9d7
notgopi <gopiinathgaonkar@gmail.com>
2024-12-03 20:35:07 +0530

    Add files via upload

--- my_bot/my_bot/follow_waypoints.py
+++ my_bot/my_bot/follow_waypoints.py
@@ -31,9 +31,16 @@ def main():
     # Inspection route, probably read in from a file for a real application
     # from either a map or drive and repeat.
     inspection_route = [ # simulation points
-        [0.0, 2.0],
-        [0.0, 0.0],
-        [0.0, -2.0]]
+        [-3.2, 3.215, 0.70710, 0.70710],
+        [-3.2, 7.965, 1.0, 0.0],
+        [-3.2, 7.965, 0.0, 1.0],
+        [-3.2, 12.215, 0.70710, 0.70710],
+        [1.3, 12.215, 0.70710, 0.70710],
+        [5.4, 12.215, 0.70710, 0.70710],
+        [10.1, 12.215, 0.0, 1.0],
+        [5.1, 12.215, -0.70710, 0.70710],
+        [-1.2, 12.215, -0.92388, 0.38268],
+        [-1.2, 3.215, 0.70710, 0.70710]]
 
 
     # Set our demo's initial pose
@@ -56,11 +63,11 @@ def main():
         inspection_pose = PoseStamped()
         inspection_pose.header.frame_id = 'map'
         inspection_pose.header.stamp = navigator.get_clock().now().to_msg()
-        inspection_pose.pose.orientation.z = 1.0
-        inspection_pose.pose.orientation.w = 0.0
         for pt in inspection_route:
             inspection_pose.pose.position.x = pt[0]
             inspection_pose.pose.position.y = pt[1]
+            inspection_pose.pose.orientation.z = pt[2]
+            inspection_pose.pose.orientation.w = pt[3]
             inspection_points.append(deepcopy(inspection_pose))
         nav_start = navigator.get_clock().now()
         navigator.followWaypoints(inspection_points)
